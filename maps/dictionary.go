ff3cc637f022cb7a9c71c8a694a68e91b7bb0a85
devKanishk15 <kanishkmankar07@gmail.com>
2025-07-24 23:50:19 +0530

    refactor - custom dictionary type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,9 +1,20 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
+var errorFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	defination, ok := d[word]
+	if !ok {
+		return "", errorFound
+	}	
+	
+	return defination, nil
 }
 
-
+func (d Dictionary) Add(word, defination string) {
+	d[word] = defination
+}
