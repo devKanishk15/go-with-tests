6d70ff2ea1ec9b2487b516d9de1cc12cd38dd912
devKanishk15 <kanishkmankar07@gmail.com>
2025-07-24 23:18:32 +0530

    refactor - custom dictionary type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -2,6 +2,8 @@ package maps
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	return d[word], nil
 }
+
+
