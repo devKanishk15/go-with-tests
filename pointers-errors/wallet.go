332cad4c16f011bf24da4cf01291fd3b5fb75327
devKanishk15 <kanishkmankar07@gmail.com>
2025-07-24 20:09:46 +0530

    implemented pointers

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -13,6 +13,13 @@ type Wallet struct {
 	w *Wallet now becomes pointer to struct Wallet
 */ 
 
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance =+ amount
